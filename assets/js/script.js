3b681902c1ff8f4cd1099f9c8c8afe3755b954db
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-07-04 15:16:27 +0200

    :tada: initial comimt

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const ageDiv = document.getElementById("age");
+const currentAge = new Date().getFullYear() - 2005;
+ageDiv.innerText = currentAge;
+
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+openMenuBtn.addEventListener("click", () => {
+    menu.classList.add("active")
+})
+
+closeMenuBtn.addEventListener("click", () => {
+    closeMenu();
+})
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        closeMenu();
+    })
+})
+
+function closeMenu() {
+    menu.classList.remove("active")
+}
