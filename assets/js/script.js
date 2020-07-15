00929d13c77646c339396a4ab1e5fc39d5fe9a59
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-07-15 09:00:43 +0200

    added contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
+const config = {
+  mailService: "default_service",
+  templateId: "template_GgjP66Zj",
+  contactForm: "#contact-form",
+};
+
 const menu = document.querySelector(".menu");
-const footerIcon = document.querySelector(".footer-icon")
+const footerIcon = document.querySelector(".footer-icon");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 const menuLinks = document.querySelectorAll(".menu-link");
 
 openMenuBtn.addEventListener("click", () => {
-    document.body.classList.add("no-scroll")
-    menu.classList.add("active")
-})
+  document.body.classList.add("no-scroll");
+  menu.classList.add("active");
+});
 
 closeMenuBtn.addEventListener("click", () => {
-    closeMenu();
-})
+  closeMenu();
+});
 
 menuLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-        closeMenu();
-    })
-})
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
 
 function closeMenu() {
-    menu.classList.remove("active")
-    document.body.classList.remove("no-scroll")
+  menu.classList.remove("active");
+  document.body.classList.remove("no-scroll");
 }
 
 const ageDiv = document.getElementById("age");
@@ -29,9 +35,30 @@ const currentAge = new Date().getFullYear() - 2005;
 ageDiv.innerText = currentAge;
 
 footerIcon.addEventListener("click", () => {
-    document.body.classList.add("rotate")
+  document.body.classList.add("rotate");
 
-    setTimeout(() => {
-        document.body.classList.remove("rotate")
-    }, 4000)
-})
+  setTimeout(() => {
+    document.body.classList.remove("rotate");
+  }, 4000);
+});
+
+// contact form
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = document.getElementById("msg");
+
+  emailjs
+    .sendForm(config.mailService, config.templateId, config.contactForm)
+    .then((res) => {
+      if (res.status === 200) {
+        message.style.display = "block";
+        message.textContent = "Successfully send message!";
+
+        setTimeout(() => {
+          message.style.display = "none";
+        }, 10000);
+      }
+    });
+});
